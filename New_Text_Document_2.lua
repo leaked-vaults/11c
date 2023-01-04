7f04eaab35237d5684aba10e803c573013d130f1
L SKIDS <118946243+leaked-vaults@users.noreply.github.com>
2023-01-04 16:42:07 -0500

    Add files via upload

---
+++ New_Text_Document_2.lua
@@ -0,0 +1,43 @@
+-- // THIS IS TO SPAM OUT THE LOGS U IT SPACES IT OUT
+wait(3)
+print("Stack Begin")
+print("Script 'Players.ROblox.PlayerGui.Framework', Line5")
+print("Stack End")
+
+
+wait(0.2)
+print("https://www.roblox.com/client-status/set?broswerTrackerId=1&status=Ingame : HttpError")
+
+
+wait(3)
+print("Stack Begin")
+print("Script 'Players.Roblox.PlayerGui.Framework', Line 2521")
+print("Stack End")
+
+wait(0.2)
+
+print("https://www.roblox.com/client-status/set?broswerTrackerId=1&status=Ingame : HttpError")
+
+wait(3)
+
+print("Stack Begin")
+print("Script 'Players.{user}.PlayerGui.Framework', Line 259")
+print("Stack End")
+
+wait(0.2)
+
+print("https://www.roblox.com/client-status/set?broswerTrackerId=1&status=Ingame : HttpError")
+
+wait(3)
+
+print("Stack Begin")
+print("Script 'Players.{user}.PlayerGui.Framework', Line2")
+print("Stack End")
+
+wait(0.2)
+
+print("https://www.roblox.com/client-status/set?broswerTrackerId=1&status=Ingame : HttpError")
+
+wait(2)
+
+print("No logs Found , Error line8")
---
+++ mhal_sets.lua
@@ -0,0 +1,47 @@
+  --[[
+    MHAL SETS FOR DOT AND SILENT
+    IF YOU WANT A NEW MHAL DOT DM ME 11c#0001
+    SETS DOWN BELOW.
+    I PUT THIS TOGETHER FOR MHAL (11c)
+                                  ]]
+
+       -- //.//Dot
+-- //.//MHAL FAR SETS AND MID
+   \\ 0.5267 ..\\
+
+
+   \\ 0.2678 ..\\
+
+    \\ 0.1267 ..\\
+     // 0.56  ..\\
+      \\0.61  ..\\
+       \\ 0.8951 ..\\
+
+--     //.//MHALMadeThisOne   VERY CLOSE SETS
+
+
+
+                    \\ 0.264212661 ..\\
+                     \\ 0.515421526 ..\\
+                      \\ 0.671      .. \\
+
+
+
+                 -- //.//Silent
+
+
+                --/./Fov
+                FOV = 15.621525
+                FOV = 20
+                FOV = 90
+                FOV = 180
+                FOV = 300
+                --/./FovSide
+                FOVside = 30
+                FOVside = 40
+                FOVside = 79.612
+                FOVside = 90
+                FOVside = 190
+                --/./FovShow
+                FOVshow = false (all ways false)
+
